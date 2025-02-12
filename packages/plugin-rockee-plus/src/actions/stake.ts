699f66adf7d0394a6273cb9572304f3bb8e54ba3
Duc LA <laminhduc2610@gmail.com>
2025-02-12 22:51:22 +0700

    update

--- packages/plugin-rockee-plus/src/actions/stake.ts
+++ packages/plugin-rockee-plus/src/actions/stake.ts
@@ -161,7 +161,7 @@ export const stakeNavi: Action = {
                 }
             }
             let data= await redis.hGet("STAKE_POOLS",symbolOnPoolNavi);
-            
+            console.log(data)
             if (data && typeof data === "object" && data !== null) {
                 callback({
                     text: "Please ensure all details are correct before proceeding with the swap to prevent any losses:",
