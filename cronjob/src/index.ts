cd2abd3f2da72f7e0df69d0c245d2382678d7de8
Duc LA <laminhduc2610@gmail.com>
2025-02-12 16:41:43 +0700

    update cronjob

--- cronjob/src/index.ts
+++ cronjob/src/index.ts
@@ -1,10 +1,11 @@
 import { JobQueue, JobWorker } from "@elizaos/adapter-bullmq"; // Đảm bảo rằng bạn có file JobWorker.ts
 import { elizaLogger } from "@elizaos/core";
 import dotenv from "dotenv";
-import {fetchTopDexByNetwork} from "./services/fetchTopDex"
+import { fetchTopDexByNetwork } from "./services/fetchTopDex";
 import { fetchNaviPool } from "./services/fetchNaviPool";
-import {getCoinAll} from "./services/fetchCoinCMS"
+import { getCoinAll } from "./services/fetchCoinCMS";
 dotenv.config();
+
 const QUEUE_NAME = process.env.QUEUE_NAME || "cronjob";
 const REDIS_URL = process.env.REDIS_URL || "redis://localhost:6379";
 
@@ -15,14 +16,20 @@ const scheduledJobs = [
     { jobName: "fetchNaviPool", data: {}, cron: "0 * * * *" }, // run every hour
     { jobName: "fetchCoinCMS", data: {}, cron: "0 * * * *" },  // run every hour
 ];
+
 (async () => {
     for (const job of scheduledJobs) {
         await jobQueue.addJob(job.jobName, job.data, { repeat: { cron: job.cron } });
     }
 })();
-const worker = new JobWorker(QUEUE_NAME, REDIS_URL);
 
-worker.registerJob("fetchSuiDex", fetchTopDexByNetwork);
-worker.registerJob("fetchNaviPool", fetchNaviPool);
-worker.registerJob("fetchCoinCMS", getCoinAll);
-elizaLogger.info("🚀 Worker & Scheduler running...");
+const startWorker = () => {
+    const worker = new JobWorker(QUEUE_NAME, REDIS_URL);
+
+    worker.registerJob("fetchSuiDex", fetchTopDexByNetwork);
+    worker.registerJob("fetchNaviPool", fetchNaviPool);
+    worker.registerJob("fetchCoinCMS", getCoinAll);
+    elizaLogger.info("🚀 Worker & Scheduler running...");
+};
+
+startWorker();
