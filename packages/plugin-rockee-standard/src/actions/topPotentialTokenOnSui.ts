134fd0dacaaac0f75064f5d77eac8d7585501132
Duc LA <laminhduc2610@gmail.com>
2025-02-18 16:38:10 +0700

    update

--- packages/plugin-rockee-standard/src/actions/topPotentialTokenOnSui.ts
+++ packages/plugin-rockee-standard/src/actions/topPotentialTokenOnSui.ts
@@ -84,7 +84,7 @@ export const topPotentialTokenOnSui: Action = {
             "0x2::sui::SUI",
             "0xdeeb7a4662eec9f2f3def03fb937a663dddaa2e215b8078a284d026b7946c270::deep::DEEP",
             "0x06864a6f921804860930db6ddbe2e16acdf8504495ea7481637a1c8b9a8fe54b::cetus::CETUS",
-            "0x09b1537c042ad9b82e66eb763a4e3b27b03b20563e47b9c062fb4e3e80d34832::navx::NAVX",
+            "0xa99b8952d4f7d947ea77fe0ecdcc9e5fc0bcab2841d6e2a5aa00c3044e5544b5::navx::NAVX",
             "0xb45fcfcc2cc07ce0702cc2d229621e046c906ef14d9b25e8e4d25f6e8763fef7::send::SEND",
             "0xe1b45a0e641b9955a20aa0ad1c1f4ad86aad8afb07296d4085e349a50e90bdca::blue::BLUE",
             "0xb4bc93ad1a07fe47943fc4d776fed31ce31923acb5bc9f92d2cab14d01fc06a4::ROCK::ROCK"
