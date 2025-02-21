746e533915624f47d449583c6fa2ba8b74d4666a
0xgloria <nimshao506@gmail.com>
2025-02-21 13:22:19 +0700

    update type navi

--- packages/plugin-rockee-plus/src/actions/stake.ts
+++ packages/plugin-rockee-plus/src/actions/stake.ts
@@ -138,6 +138,7 @@ export const stake: Action = {
                         responseData[index].base_borrow_rate = poolInfo.base_borrow_rate;
                         responseData[index].boosted_supply_rate = poolInfo.boosted_supply_rate;
                         responseData[index].boosted_borrow_rate = poolInfo.boosted_borrow_rate;
+                        responseData[index].type = "navi";
 
                     } else {
                         elizaLogger.error(`Pool information for key ${key} is undefined.`);
