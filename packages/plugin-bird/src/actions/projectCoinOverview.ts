8969a2659538a187fcfe2a22cfc79108a9cf68a2
0xgloria <nimshao506@gmail.com>
2025-02-26 14:17:57 +0700

    update project overview

--- packages/plugin-bird/src/actions/projectCoinOverview.ts
+++ packages/plugin-bird/src/actions/projectCoinOverview.ts
@@ -206,7 +206,7 @@ export const projectInfo: Action = {
             infoPrice = { market_cap_rank: "N/A", price_change_24h: "N/A", price: tokenSuiInfo.tokenPrice, market_cap: tokenSuiInfo.marketCap };
             infoDetail = { market_cap_rank: "N/A", tickers: [] };
             const coinGecko = new CoingeckoProvider();
-            if (tokenSuiInfo.coingecko_coin_id && tokenObject.coinGeckoId) {
+            if (tokenSuiInfo.coingecko_coin_id || tokenObject.coinGeckoId) {
                 getToken = await coinGecko.getToken(tokenSuiInfo && tokenSuiInfo.coingecko_coin_id && tokenSuiInfo.coingecko_coin_id !== null ? tokenSuiInfo.coingecko_coin_id : tokenObject.coinGeckoId || "");
                 getDetail = await coinGecko.getCoinDataById(tokenSuiInfo && tokenSuiInfo.coingecko_coin_id && tokenSuiInfo.coingecko_coin_id !== null ? tokenSuiInfo.coingecko_coin_id : tokenObject.coinGeckoId || "");
             }
