39065947d4e9b3fa573dfda4459e42edcfbc900e
0xgloria <nimshao506@gmail.com>
2025-02-26 16:54:20 +0700

    update stake

--- packages/plugin-rockee-plus/src/actions/stake.ts
+++ packages/plugin-rockee-plus/src/actions/stake.ts
@@ -441,9 +441,7 @@ export const stake: Action = {
                     if (content.pool_name === null || content.pool_name === "null") {
                         content.pool_name = "sui"
                     }
-                    console.log("dataScallop:", dataScallop);
                     dataScallop = await redis.hGet("STAKE_POOLS_SCALLOP", content.pool_name.toLowerCase());
-                    console.log("dataScallop:", dataScallop);
                     if (dataScallop && typeof dataScallop === "string" && dataScallop !== null) {
                         callback({
                             user: await runtime.character.name,
