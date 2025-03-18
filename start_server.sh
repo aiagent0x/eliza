1c6d81e15cdbe16d7f58723e42db86022f17ae2a
0xgloria <nimshao506@gmail.com>
2025-03-18 11:32:01 +0700

    update start_server

--- start_server.sh
+++ start_server.sh
@@ -1,3 +1,3 @@
 
 #!/bin/bash
-pnpm build && pnpm start --characters="characters/rockee_plus.json, characters/rockee_standard.json, character/birds_sui.json, characters/rockee_monad.json"
+pnpm build && pnpm start --characters="characters/rockee_plus.json, characters/rockee_standard.json, characters/rockee_monad.json"
