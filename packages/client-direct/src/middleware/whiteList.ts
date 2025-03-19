397320ff72e08b562746ce2c8c9b68fd12210e92
0xgloria <nimshao506@gmail.com>
2025-03-19 19:06:52 +0700

    update whitelist

--- packages/client-direct/src/middleware/whiteList.ts
+++ packages/client-direct/src/middleware/whiteList.ts
@@ -1,6 +1,6 @@
 import { Request, Response, NextFunction } from 'express';
 
-const whiteList = ["13.251.248.206", "123.25.21.181", "127.0.0.1", "http://localhost:3003"]
+const whiteList = ["13.251.248.206", "123.25.21.181", "127.0.0.1", "http://localhost:3003","171.224.179.110","116.99.34.73"]
 export default function  whiteListMiddleware (req: Request, res: Response, next: NextFunction) {
     const clientIp = req.ip;
     console.log(req.ip)
