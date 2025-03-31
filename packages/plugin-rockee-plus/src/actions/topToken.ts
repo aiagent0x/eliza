5d552cfc4e6701c95f8c7aed253c752571f4a4d4
0xgloria <nimshao506@gmail.com>
2025-03-31 20:24:16 +0700

    update

--- packages/plugin-rockee-plus/src/actions/topToken.ts
+++ packages/plugin-rockee-plus/src/actions/topToken.ts
@@ -589,7 +589,7 @@ export const topToken: Action = {
                 case "LISTING":
                     callback({
                         user: await runtime.character.name,
-                        text: `Top tokens that are about to have their TGE, token release, or exchange listing: BIRDS, SEED, FANTV, Wave, Haedal, 7K`,
+                        text: `Top tokens that are about to have their TGE, token release, or exchange listing: BIRDS, SEED, FANTV, Haedal, 7K`,
                     })
                     return true;
                     break;
