0e41f3e02889339babae6e1df4b140e454302481
0xgloria <nimshao506@gmail.com>
2025-04-28 16:45:28 +0700

    Update sample pool names for Suilend in staking parameters

--- packages/plugin-rockee-plus/src/actions/stake.ts
+++ packages/plugin-rockee-plus/src/actions/stake.ts
@@ -30,7 +30,7 @@ Extract the staking parameters from the latest message only, following these rul
 
 - Sample Pool Names Navi: SUI, USDT, WETH, CETUS, VoloSui, HaedalSui, **NAVX**, WBTC, AUSD, wUSDC, nUSDC, ETH, USDY, NS, stBTC, DEEP, FDUSD, BLUE, BUCK, suiUSDT, stSUI, **WAL**.  
 - Sample Pool Names Scallop: usdc, sbeth, sbusdt, sbwbtc, weth, wbtc, wusdc, wusdt, sui, wapt, wsol, cetus, afsui, hasui, vsui, sca, fud, deep, fdusd, blub, "musd, **WAL**.
-- Sample Pool Names Suilend: SUI, **SEND**, WAL, DEEP, mUSD, suiUSDT, wBTC, AUSD, trevinSUI, LBTC, sSUI, USDC, BUCK, SOL, upSUI, fudSUI, mSUI, kSUI, suiETH, wUSDT, HIPPO, HIPPO, wUSDC, WETH, NS, BLUE, FUD, yapSUI, iSUI.
+- Sample Pool Names Suilend: SUI, **SEND**, **WAL**, DEEP, mUSD, suiUSDT, wBTC, AUSD, trevinSUI, LBTC, sSUI, USDC, BUCK, SOL, upSUI, fudSUI, mSUI, kSUI, suiETH, wUSDT, HIPPO, HIPPO, wUSDC, WETH, NS, BLUE, FUD, yapSUI, iSUI.
 - **Extract data only from the latest message** and discard any previous messages.  
 - Return only a **single JSON object** with the specified fields in this format: 
     \`\`\`json
