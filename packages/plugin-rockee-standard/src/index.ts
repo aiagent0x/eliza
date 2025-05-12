e432fdf6842b588fb3c013c54e1af286f4365ce1
0xgloria <nimshao506@gmail.com>
2025-05-12 10:31:53 +0700

    fix: remove unused imports and commented-out actions from index.ts

--- packages/plugin-rockee-standard/src/index.ts
+++ packages/plugin-rockee-standard/src/index.ts
@@ -1,12 +1,8 @@
 import { Plugin } from '@elizaos/core';
 import { suiTokenPriceByAddress } from './actions/suiTokenPriceByAddress';
-import { executeSwap } from './actions/swapBySymbol';
-import { executeSwapByAddress } from './actions/swapByAddress';
 import { sendTokenBySymbol } from './actions/sendTokenBySymbol';
-import { checkTxhashOnSui } from './actions/checkInfoTxHash';
 import { projectInfo } from './actions/projectCoinOverview';
 import { suiTokenPriceBySymbol } from './actions/suiTokenPriceBySymbol';
-import { topPotentialTokenOnSui } from './actions/topPotentialTokenOnSui';
 import { swapSui } from './actions/swap';
 import { questInfo } from './actions/quest';
 import { taggingProvider } from './providers/taggingProvider';
@@ -16,15 +12,10 @@ const rockeeStandardPlugin: Plugin = {
   description: "Everything about rockee standard",
   actions: [
     swapSui,
-    // executeSwap,
-    // executeSwapByAddress,
     sendTokenBySymbol,
-    // checkTxhashOnSui,
     projectInfo,
-    // topDexInfo,
     suiTokenPriceBySymbol,
     suiTokenPriceByAddress,
-    // topPotentialTokenOnSui
     questInfo
 ],
   evaluators: [],
