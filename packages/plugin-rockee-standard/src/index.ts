04e717d20573e2e2f0fc1633c284da9e3295fe6d
Gloria <gloria@gmail.com>
2025-06-03 14:25:15 +0700

    fix: improve code formatting and consistency in index.ts

--- packages/plugin-rockee-standard/src/index.ts
+++ packages/plugin-rockee-standard/src/index.ts
@@ -1,26 +1,25 @@
-import { Plugin } from '@elizaos/core';
-import { suiTokenPriceByAddress } from './actions/suiTokenPriceByAddress';
-import { sendTokenBySymbol } from './actions/sendTokenBySymbol';
-import { projectInfo } from './actions/projectCoinOverview';
-import { suiTokenPriceBySymbol } from './actions/suiTokenPriceBySymbol';
-import { swapSui } from './actions/swap';
-import { questInfo } from './actions/quest';
-import { taggingProvider } from './providers/taggingProvider';
+import { Plugin } from "@elizaos/core";
+import { suiTokenPriceByAddress } from "./actions/suiTokenPriceByAddress";
+import { sendTokenBySymbol } from "./actions/sendTokenBySymbol";
+import { projectInfo } from "./actions/projectCoinOverview";
+import { suiTokenPriceBySymbol } from "./actions/suiTokenPriceBySymbol";
+import { swapSui } from "./actions/swap";
+import { questInfo } from "./actions/quest";
+import { taggingProvider } from "./providers/taggingProvider";
 
 const rockeeStandardPlugin: Plugin = {
-  name: "rockeeStandardPlugin",
-  description: "Everything about rockee standard",
-  actions: [
-    swapSui,
-    sendTokenBySymbol,
-    projectInfo,
-    suiTokenPriceBySymbol,
-    suiTokenPriceByAddress,
-    questInfo
-],
-  evaluators: [],
-  providers: [taggingProvider]
+    name: "rockeeStandardPlugin",
+    description: "Everything about rockee standard",
+    actions: [
+        swapSui,
+        sendTokenBySymbol,
+        projectInfo,
+        suiTokenPriceBySymbol,
+        suiTokenPriceByAddress,
+        questInfo,
+    ],
+    evaluators: [],
+    providers: [taggingProvider],
 };
 
 export default rockeeStandardPlugin;
-
